be8b1a6e9be00c70ea486b495ea7e38cc3a66e34
Edward <pskwong@outlook.com>
2024-08-21 13:05:03 +0100

    Access Token V2

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,8 +1,13 @@
-import React, {useEffect, useState } from 'react'; 
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
-import AppStyle from './AppContainer.module.css'; 
+import AppStyle from './AppContainer.module.css';
 
+import Spotify from '../../API/Spotify';
+
+import { getAccessToken, extractAccessToken } from '../../API/Spotify';
+
+import { AuthenComponent } from '../Authentication/authenComponent';
 import { HeaderComponent } from '../Header/headerComponent';
 import { SearchingContainer } from '../Searching/SearchingComponent';
 
@@ -10,16 +15,23 @@ import { SearchingContainer } from '../Searching/SearchingComponent';
 
 function App() {
 
+  extractAccessToken(); 
+
+
   //Define the "Searching Value" for the App 
   const [inputValue, setInputValue] = useState('Input name of your favourite track /artist /album');
+
+  const [authenStatus, setAuthenStatus] = useState(false)
 
 
 
   return (
     <div className={AppStyle.AppContainer}>
       <HeaderComponent />
+      <AuthenComponent getAccessToken={getAccessToken} />
       <div className={AppStyle.functionContainer}>
-          <SearchingContainer inputValue={inputValue} setInputValue={setInputValue} />
+        <SearchingContainer inputValue={inputValue} setInputValue={setInputValue} />
+        <input type='input' ></input>
 
       </div>
     </div>
